cc8c1e4fd00ce8afa3d92bdd1d235522cfc2f93b
githubnull <githubnull@github.com>
2026-06-29 23:18:21 +0800

    fix(ci): 修复 release notes 脚本兼容性，添加 .gitattributes 强制 LF 换行

--- .github/scripts/gen-release-notes.sh
+++ .github/scripts/gen-release-notes.sh
@@ -26,26 +26,34 @@ if [[ "$CURRENT_TAG" == "HEAD" ]]; then
   RANGE="-20"
 fi
 
-declare -A GROUPS=(
-  [feat]="### 新功能"
-  [fix]="### Bug 修复"
-  [perf]="### 性能优化"
-  [refactor]="### 重构"
-  [docs]="### 文档"
-  [test]="### 测试"
-  [build]="### 构建"
-  [ci]="### CI"
-  [chore]="### 杂项"
-  [style]="### 样式"
-  [revert]="### 回滚"
-)
-ORDER=(feat fix perf refactor docs test build ci chore style revert)
+GROUP_FEAT="### 新功能"
+GROUP_FIX="### Bug 修复"
+GROUP_PERF="### 性能优化"
+GROUP_REFACTOR="### 重构"
+GROUP_DOCS="### 文档"
+GROUP_TEST="### 测试"
+GROUP_BUILD="### 构建"
+GROUP_CI="### CI"
+GROUP_CHORE="### 杂项"
+GROUP_STYLE="### 样式"
+GROUP_REVERT="### 回滚"
+GROUP_OTHER="### 其他"
 
 # 读入提交：格式 "<short-sha>|<subject>"
 mapfile -t LINES < <(git log --no-merges --pretty=format:'%h|%s' "$RANGE")
 
-declare -A BUCKETS
-OTHERS=()
+BUCKET_FEAT=""
+BUCKET_FIX=""
+BUCKET_PERF=""
+BUCKET_REFACTOR=""
+BUCKET_DOCS=""
+BUCKET_TEST=""
+BUCKET_BUILD=""
+BUCKET_CI=""
+BUCKET_CHORE=""
+BUCKET_STYLE=""
+BUCKET_REVERT=""
+OTHERS=""
 
 for line in "${LINES[@]}"; do
   sha="${line%%|*}"
@@ -56,33 +64,40 @@ for line in "${LINES[@]}"; do
     scope="${BASH_REMATCH[2]}"
     desc="${BASH_REMATCH[3]}"
     item="- ${scope:+**${scope//[()]/}**: }${desc} (${sha})"
-    if [[ -n "${GROUPS[$type]:-}" ]]; then
-      BUCKETS[$type]+="${item}"$'\n'
-    else
-      OTHERS+=("- ${msg} (${sha})")
-    fi
+    case "$type" in
+      feat)     BUCKET_FEAT+="${item}"$'\n' ;;
+      fix)      BUCKET_FIX+="${item}"$'\n' ;;
+      perf)     BUCKET_PERF+="${item}"$'\n' ;;
+      refactor) BUCKET_REFACTOR+="${item}"$'\n' ;;
+      docs)     BUCKET_DOCS+="${item}"$'\n' ;;
+      test)     BUCKET_TEST+="${item}"$'\n' ;;
+      build)    BUCKET_BUILD+="${item}"$'\n' ;;
+      ci)       BUCKET_CI+="${item}"$'\n' ;;
+      chore)    BUCKET_CHORE+="${item}"$'\n' ;;
+      style)    BUCKET_STYLE+="${item}"$'\n' ;;
+      revert)   BUCKET_REVERT+="${item}"$'\n' ;;
+      *)        OTHERS+="- ${msg} (${sha})"$'\n' ;;
+    esac
   else
-    OTHERS+=("- ${msg} (${sha})")
+    OTHERS+="- ${msg} (${sha})"$'\n'
   fi
 done
 
 {
   echo "## ${CURRENT_TAG}"
   echo
-  for t in "${ORDER[@]}"; do
-    if [[ -n "${BUCKETS[$t]:-}" ]]; then
-      echo "${GROUPS[$t]}"
-      echo
-      echo -n "${BUCKETS[$t]}"
-      echo
-    fi
-  done
-  if (( ${#OTHERS[@]} > 0 )); then
-    echo "### 其他"
-    echo
-    printf '%s\n' "${OTHERS[@]}"
-    echo
-  fi
+  if [[ -n "$BUCKET_FEAT" ]]; then     echo "$GROUP_FEAT"; echo; echo -n "$BUCKET_FEAT"; echo; fi
+  if [[ -n "$BUCKET_FIX" ]]; then      echo "$GROUP_FIX"; echo; echo -n "$BUCKET_FIX"; echo; fi
+  if [[ -n "$BUCKET_PERF" ]]; then     echo "$GROUP_PERF"; echo; echo -n "$BUCKET_PERF"; echo; fi
+  if [[ -n "$BUCKET_REFACTOR" ]]; then echo "$GROUP_REFACTOR"; echo; echo -n "$BUCKET_REFACTOR"; echo; fi
+  if [[ -n "$BUCKET_DOCS" ]]; then     echo "$GROUP_DOCS"; echo; echo -n "$BUCKET_DOCS"; echo; fi
+  if [[ -n "$BUCKET_TEST" ]]; then     echo "$GROUP_TEST"; echo; echo -n "$BUCKET_TEST"; echo; fi
+  if [[ -n "$BUCKET_BUILD" ]]; then    echo "$GROUP_BUILD"; echo; echo -n "$BUCKET_BUILD"; echo; fi
+  if [[ -n "$BUCKET_CI" ]]; then       echo "$GROUP_CI"; echo; echo -n "$BUCKET_CI"; echo; fi
+  if [[ -n "$BUCKET_CHORE" ]]; then    echo "$GROUP_CHORE"; echo; echo -n "$BUCKET_CHORE"; echo; fi
+  if [[ -n "$BUCKET_STYLE" ]]; then    echo "$GROUP_STYLE"; echo; echo -n "$BUCKET_STYLE"; echo; fi
+  if [[ -n "$BUCKET_REVERT" ]]; then   echo "$GROUP_REVERT"; echo; echo -n "$BUCKET_REVERT"; echo; fi
+  if [[ -n "$OTHERS" ]]; then          echo "$GROUP_OTHER"; echo; echo -n "$OTHERS"; echo; fi
   if [[ -n "$COMPARE_URL_LINE" ]]; then
     echo "$COMPARE_URL_LINE"
   fi
